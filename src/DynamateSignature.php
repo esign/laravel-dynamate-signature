7e0e8c149f64def5d8c69b3eea4d5f73d0e19bac
jordyvanderhaegen <jordy@esign.eu>
2025-01-29 12:10:47 +0100

    chore: remove redundant package files

--- src/DynamateSignature.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Esign\DynamateSignature;
-
-class DynamateSignature
-{
-}
--- src/DynamateSignatureServiceProvider.php
+++ src/DynamateSignatureServiceProvider.php
@@ -8,22 +8,9 @@ class DynamateSignatureServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([$this->configPath() => config_path('dynamate-signature.php')], 'config');
-        }
     }
 
     public function register()
     {
-        $this->mergeConfigFrom($this->configPath(), 'dynamate-signature');
-
-        $this->app->singleton('dynamate-signature', function () {
-            return new DynamateSignature;
-        });
-    }
-
-    protected function configPath(): string
-    {
-        return __DIR__ . '/../config/dynamate-signature.php';
     }
 }
